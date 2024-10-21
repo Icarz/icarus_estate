a443ddc5ca817f68c8fd5d98142ebc50e1d5ce72
Icarz <mr.rghay@email.com>
2024-10-21 16:11:03 +0100

    error_handling

--- api/index.js
+++ api/index.js
@@ -21,3 +21,15 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+// creating middleware //
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});

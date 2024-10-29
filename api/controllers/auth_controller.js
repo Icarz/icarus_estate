31c8e894ee48d302a89eff060acf02dcbf8c3c5f
Icarz <mr.rghay@email.com>
2024-10-29 18:11:34 +0100

    auth_sign_in

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user_model.js";
 import bcryptjs from "bcryptjs";
+import { errorhandler } from "../utils/errors.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -11,4 +12,15 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body; // we want a request from the body to have the email and password//
+  try {
+    const validUser = await User.findOne({ email }); // creating the constant to work on //
+    if (!validUser) return next(errorhandler(404, "user not found")); // validation in database//
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorhandler(401, "wrong credentials!")); // validation in database
+  } catch (error) {}
+  next(error);
 };

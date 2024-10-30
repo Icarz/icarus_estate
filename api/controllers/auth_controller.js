cdf1ca6069dc8cc76dea6cf203934e52405a4c9e
Icarz <mr.rghay@email.com>
2024-10-30 15:15:30 +0100

    creating the sigin api done!

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user_model.js";
 import bcryptjs from "bcryptjs";
 import { errorhandler } from "../utils/errors.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -17,10 +18,17 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
   const { email, password } = req.body; // we want a request from the body to have the email and password//
   try {
-    const validUser = await User.findOne({ email }); // creating the constant to work on //
+    const validUser = await User.findOne({ email }); // creating the constant to work on, with findOne method //
     if (!validUser) return next(errorhandler(404, "user not found")); // validation in database//
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorhandler(401, "wrong credentials!")); // validation in database
-  } catch (error) {}
-  next(error);
+    const token = jwt.sign({ id: validUser.id }, process.env.JWT_SECRET_KEY); // creating the token//
+    const { password: pass, ...rest } = validUser._doc; // display the rest but not the password//
+    res // creating the cookie session//
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest); // return the json body//
+  } catch (error) {
+    next(error);
+  }
 };
